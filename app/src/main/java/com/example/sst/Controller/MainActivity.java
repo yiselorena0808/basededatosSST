c307d8ea8ccdc1b37475eb64a0646f34dc4809b3
yiselorena0808 <yiselorenasalzar@gmail.com>
2025-04-10 17:54:50 -0500

    cambios de estilo

--- app/src/main/java/com/example/sst/Controller/MainActivity.java
+++ app/src/main/java/com/example/sst/Controller/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.sst.Controller;
+import android.annotation.SuppressLint;
 import android.content.ContentValues;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.example.sst.Model.DBHelper;
@@ -13,59 +16,39 @@ import com.example.sst.R;
 
 public class MainActivity extends AppCompatActivity {
     private DBHelper dbHelper;
-    private EditText etIdGestion, etIdUsuario, etIdUsuario2, etNombre, etApellido, etCedula, etCargo, etProducto, etCantidad, etEstado, etImportancia;
+    private EditText  etNombre,etCedula,etCargo,etLugar,etdescripcion,etfecha;
+    private Spinner sp_importancia;
     private Button btnGuardar;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         dbHelper = new DBHelper(this);
-
-        etIdGestion = findViewById(R.id.et_id_gestion);
-        etIdUsuario = findViewById(R.id.et_id_usuario);
-        etIdUsuario2 = findViewById(R.id.et_id_usuario2);
         etNombre = findViewById(R.id.et_nombre);
-        etApellido = findViewById(R.id.et_apellido);
         etCedula = findViewById(R.id.et_cedula);
         etCargo = findViewById(R.id.et_cargo);
-        etProducto = findViewById(R.id.et_producto);
-        etCantidad = findViewById(R.id.et_cantidad);
-        etEstado = findViewById(R.id.et_estado);
-        etImportancia = findViewById(R.id.et_importancia);
+        etdescripcion= findViewById(R.id.et_descripcion);
+        etLugar= findViewById(R.id.et_lugar);
 
-        btnGuardar = findViewById(R.id.btn_guardar);
-        btnGuardar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                guardarDatos();
-            }
-        });
+        sp_importancia= findViewById(R.id.sp_importancia);
+        ArrayAdapter<CharSequence> adapterImportancia = ArrayAdapter.createFromResource(this, R.array.importancias, android.R.layout.simple_spinner_item);
+        adapterImportancia.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sp_importancia.setAdapter(adapterImportancia);
+
+
     }
 
     private void guardarDatos() {
-        String idGestion = etIdGestion.getText().toString();
-        String idUsuario = etIdUsuario.getText().toString();
-        String idUsuario2 = etIdUsuario2.getText().toString();
         String nombre = etNombre.getText().toString();
-        String apellido = etApellido.getText().toString();
         String cedula = etCedula.getText().toString();
         String cargo = etCargo.getText().toString();
-        String producto = etProducto.getText().toString();
-        String cantidad = etCantidad.getText().toString();
-        String estado = etEstado.getText().toString();
-        String importancia = etImportancia.getText().toString();
 
         SQLiteDatabase bd= dbHelper.getWritableDatabase();
         ContentValues values= new ContentValues();
-        values.put("id_gestion", idGestion);
-        values.put("id_usuario", idUsuario);
-        values.put("id_usuario2", idUsuario2);
         values.put("nombre", nombre);
-        values.put("apellido", apellido);
         values.put("cedula", cedula);
         values.put("cargo", cargo);
-        values.put("producto", producto);
-        values.put("cantidad", cantidad);
         values.put("estado", estado);
         values.put("importancia", importancia);
         bd.insert("gestion_equipos", null, values);
--- app/src/main/java/com/example/sst/Model/Constantes.java
+++ app/src/main/java/com/example/sst/Model/Constantes.java
@@ -2,8 +2,8 @@ package com.example.sst.Model;
 
 public class Constantes {
     public static  String DB_NAME = "gestion_equipos.db";
-    public static int DB_VERSION = 1;
+    public static int DB_VERSION = 2;
 
-    public static String GESTION_EPP="Create table gestiones(id_gestion INTEGER PRIMARY KEY,id_usuario INTEGER,id_usuario2 INTEGER,nombre TEXT,apellido TEXT,cedula TEXT,cargo TEXT,producto TEXT,cantidad INTEGER,estado TEXT,importancia TEXT)";
+    public static String GESTION_EPP="Create table gestiones(nombre TEXT,cedula TEXT,cargo TEXT,lugar TEXT,descripcion TEXT,estado TEXT)";
 
 }
