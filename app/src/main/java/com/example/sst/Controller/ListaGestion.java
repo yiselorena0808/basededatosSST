4302f2b6338c84c66506ba332fe5bd2193d73bbd
USUARIO <miguelanmartinez717@gmail.com>
2025-04-21 17:40:28 -0500

    todo

--- app/src/main/java/com/example/sst/Controller/ListaGestion.java
+++ app/src/main/java/com/example/sst/Controller/ListaGestion.java
@@ -15,12 +15,8 @@ public class ListaGestion extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+
         setContentView(R.layout.lista_gestion);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
 }
--- app/src/main/java/com/example/sst/Controller/MainActivity.java
+++ app/src/main/java/com/example/sst/Controller/MainActivity.java
@@ -10,51 +10,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.sst.R;
 import com.example.sst.ReportesRecibidos;
+import com.example.sst.databinding.ActivityMainBinding;
+
 
 public class MainActivity extends AppCompatActivity {
-    Button btnGestionEpp, btnReportes, btnActividades, btnLista, btnBlog, btnAsesoramiento;
 
+   ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        btnGestionEpp = findViewById(R.id.btn_GestionEpp);
-        btnReportes = findViewById(R.id.btn_Reportes);
-        btnActividades = findViewById(R.id.btn_Act);
-        btnLista = findViewById(R.id.btn_Lista);
-        btnBlog = findViewById(R.id.btn_Blog);
-        btnAsesoramiento = findViewById(R.id.btn_Asesoramiento);
 
-        btnGestionEpp.setOnClickListener(new View.OnClickListener() {
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        binding.btnGestionEpp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, GestionEpp.class));
             }
         });
 
-        btnReportes.setOnClickListener(new View.OnClickListener() {
+        binding.btnReportes.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, ReportesRecibidos.class));
             }
         });
 
-        btnActividades.setOnClickListener(new View.OnClickListener() {
+        binding.btnAct.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, Actividades_Ludicasmovil.class));
             }
         });
 
-        btnLista.setOnClickListener(new View.OnClickListener() {
+        binding.btnLista.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, ListaChequeo.class));
             }
         });
 
-        btnBlog.setOnClickListener(new View.OnClickListener() {
+        binding.btnBlog.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, Blog.class));
