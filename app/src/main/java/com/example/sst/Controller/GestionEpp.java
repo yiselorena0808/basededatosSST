6240537df84f1adc020e97d2e01e1741e8f1c3c1
yiselorena0808 <cfrcadsom@gmail.com>
2025-04-21 10:18:42 -0500

    gestion epp agregado2

--- app/src/main/java/com/example/sst/Controller/GestionEpp.java
+++ app/src/main/java/com/example/sst/Controller/GestionEpp.java
@@ -15,45 +15,5 @@ import com.example.sst.Model.DBHelper;
 import com.example.sst.R;
 
 public class GestionEpp extends AppCompatActivity {
-    private DBHelper dbHelper;
-    private EditText  etNombre,etCedula,etCargo,etLugar,etdescripcion,etfecha;
-    private Spinner sp_importancia;
-    private Button btnGuardar;
-    @SuppressLint("MissingInflatedId")
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.gestion_epp);
-        dbHelper = new DBHelper(this);
-        etNombre = findViewById(R.id.et_nombre);
-        etCedula = findViewById(R.id.et_cedula);
-        etCargo = findViewById(R.id.et_cargo);
-        etdescripcion= findViewById(R.id.et_descripcion);
-        etLugar= findViewById(R.id.et_lugar);
 
-        sp_importancia= findViewById(R.id.sp_importancia);
-        ArrayAdapter<CharSequence> adapterImportancia = ArrayAdapter.createFromResource(this, R.array.importancias, android.R.layout.simple_spinner_item);
-        adapterImportancia.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        sp_importancia.setAdapter(adapterImportancia);
-
-
-    }
-
-    private void guardarDatos() {
-        String nombre = etNombre.getText().toString();
-        String cedula = etCedula.getText().toString();
-        String cargo = etCargo.getText().toString();
-
-        SQLiteDatabase bd= dbHelper.getWritableDatabase();
-        ContentValues values= new ContentValues();
-        values.put("nombre", nombre);
-        values.put("cedula", cedula);
-        values.put("cargo", cargo);
-        values.put("estado", estado);
-        values.put("importancia", importancia);
-        bd.insert("gestion_equipos", null, values);
-        bd.close();
-
-        Toast.makeText(this,"Datos guardados correctamente",Toast.LENGTH_SHORT).show();
-    }
 }
---
+++ app/src/main/java/com/example/sst/Controller/ListaGestion.java
@@ -0,0 +1,26 @@
+package com.example.sst.Controller;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.sst.R;
+
+public class ListaGestion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.lista_gestion);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
