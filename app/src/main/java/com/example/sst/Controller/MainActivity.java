605e4d3cf92ba4dfe15156f5d7feb4e08ae1d4d9
sebastiancotaz12 <sebasguacas199@gmail.com>
2025-05-05 17:54:28 -0500

    nuevo

--- app/src/main/java/com/example/sst/Controller/MainActivity.java
+++ app/src/main/java/com/example/sst/Controller/MainActivity.java
@@ -3,14 +3,12 @@ package com.example.sst.Controller;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 
 import com.example.sst.Blog1;
 import com.example.sst.R;
-import com.example.sst.ReportesRecibidos;
 import com.example.sst.databinding.ActivityMainBinding;
 
 
