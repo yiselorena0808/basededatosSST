fa4981e2617baca3729591395efc68fd4ca5bd2a
sebastiancotaz12 <sebasguacas199@gmail.com>
2025-05-05 15:57:28 -0500

    nuevo

---
+++ app/src/main/java/com/example/sst/lectorreportes.java
@@ -0,0 +1,20 @@
+package com.example.sst;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class lectorreportes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_lectorreportes);
+
+    }
+}
